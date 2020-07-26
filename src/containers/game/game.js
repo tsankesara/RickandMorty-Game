c91f7ff1aa5bcaf7ebb97ef37bdd254ac680e49f
Teerth Sankesara <teerthsankesara@gmail.com>
2020-07-26 14:26:38 +0530

    bug fixed

--- src/containers/game/game.js
+++ src/containers/game/game.js
@@ -39,23 +39,29 @@ class Game extends Component{
         const ccurr_data = [...this.state.data];
         const names = [...this.state.names]
         if(ccurr_data[personIndex].name === this.state.curr_name){
+
             this.setState({counter: this.state.counter +1})
-            console.log('Counter + 1')
+            ccurr_data.splice(personIndex, 1);
+            const indexoFName = this.state.names.findIndex((a) => {
+            return a === this.state.curr_name
+        });
+            names.splice(indexoFName, 1)
+            const randomEle = names[Math.floor(Math.random() * names.length)]
+            this.setState({data: ccurr_data, names: names, curr_name: randomEle});
+
         }
         else{
             this.setState({counter: this.state.counter - 1 })
-            console.log('Counter - 1')
+            alert('Wrong Answere.')
+            const indexofWrongName = this.state.names.findIndex((a)=>{
+                return a === names[personIndex]
+            })
+            console.log(indexofWrongName)
+            names.splice(indexofWrongName, 1);
+            ccurr_data.splice(personIndex, 1);
+            const randomEle = names[Math.floor(Math.random() * names.length)]
+            this.setState({data: ccurr_data, names: names, curr_name: randomEle});
         }
-        ccurr_data.splice(personIndex, 1);
-        const indexoFName = this.state.names.findIndex((a) => {
-            return a === this.state.curr_name
-        });
-        names.splice(indexoFName, 1)
-        const randomEle = names[Math.floor(Math.random() * names.length)]
-        console.log(this.state.names)
-        this.setState({data: ccurr_data, names: names, curr_name: randomEle});
-        
-        console.log(names)
       }
     render(){
         const divResstyle ={'color': '#04bef1', 
@@ -71,7 +77,8 @@ class Game extends Component{
     <h1><span style={{'color': 'black', 'fontWeight': 'bold'}}>Score: </span> {this.state.counter}</h1> 
     <h3>Created By- Teerth Sankesara</h3></div>
     
-    <div className={style.gridd} ><Grid clicked={this.clicked} data={this.state.data}/></div></>);
+    <div className={style.gridd} ><Grid clicked={this.clicked} data={this.state.data}/></div>
+    {console.log(this.state.names, '\n')}</>);
 
     }
 
